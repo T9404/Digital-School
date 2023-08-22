fd3be16ed09c64160fcc8c4a1cd3c2cc24c16f89
T9404 <ghubman1@gmail.com>
2023-08-22 21:11:59 +0500

    added annotation to auth controller

--- auth-server/src/main/java/com/example/authserver/controller/AuthController.java
+++ auth-server/src/main/java/com/example/authserver/controller/AuthController.java
@@ -79,33 +79,111 @@ public class AuthController {
         return ResponseEntity.ok(userService.checkUsernameInUse(username));
     }
 
+    @Operation(summary = "api.auth.resendToken.summary")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "api.auth.resendToken.200.description",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = DefaultResponse.class))}),
+            @ApiResponse(responseCode = "500", description = "api.auth.500.description",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorControllerAdvice.class))})
+    })
     @GetMapping("register/resendToken")
     public ResponseEntity<DefaultResponse> resendRegistrationToken(@RequestParam("token") @Valid String token) {
         return ResponseEntity.ok(authService.resendToken(token));
     }
 
+    @Operation(summary = "api.auth.login.summary")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "api.auth.login.200.description",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AuthResponse.class))}),
+            @ApiResponse(responseCode = "404", description = "api.auth.login.404.description",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorControllerAdvice.class))}),
+            @ApiResponse(responseCode = "409", description = "api.auth.login.409.description",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorControllerAdvice.class))}),
+            @ApiResponse(responseCode = "500", description = "api.auth.500.description",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorControllerAdvice.class))})
+    })
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> authenticateUser(@RequestBody @Valid AuthRequest loginRequest) {
         return ResponseEntity.ok(authService.generateTokens(loginRequest));
     }
 
+    @Operation(summary = "api.auth.refresh.summary")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "api.auth.refresh.200.description",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AuthResponse.class))}),
+            @ApiResponse(responseCode = "404", description = "api.auth.refresh.404.description",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorControllerAdvice.class))}),
+            @ApiResponse(responseCode = "500", description = "api.auth.500.description",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorControllerAdvice.class))})
+    })
     @PostMapping("/refresh")
     public ResponseEntity<AuthResponse> updateTokens(@RequestBody @Valid RefreshRequest refresh) {
         return ResponseEntity.ok(authService.updateTokens(refresh.refreshToken()));
     }
 
+    @Operation(summary = "api.auth.confirm-email.summary")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "api.auth.confirm-email.200.description",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = DefaultResponse.class))}),
+            @ApiResponse(responseCode = "404", description = "api.auth.confirm-email.404.description",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorControllerAdvice.class))}),
+            @ApiResponse(responseCode = "409", description = "api.auth.confirm-email.409.description",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorControllerAdvice.class))}),
+            @ApiResponse(responseCode = "410", description = "api.auth.confirm-email.410.description",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorControllerAdvice.class))}),
+            @ApiResponse(responseCode = "500", description = "api.auth.500.description",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorControllerAdvice.class))})
+    })
     @GetMapping("/email/confirm")
     public ResponseEntity<DefaultResponse> confirmRegister(@RequestParam String token,
                                                            @RequestParam String email) {
         return ResponseEntity.ok(authService.confirmRegister(token, email));
     }
 
+    @Operation(summary = "api.auth.password.link.summary")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "api.auth.password.200.description",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = DefaultResponse.class))}),
+            @ApiResponse(responseCode = "404", description = "api.auth.password.404.description",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorControllerAdvice.class))}),
+            @ApiResponse(responseCode = "500", description = "api.auth.500.description",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorControllerAdvice.class))})
+    })
     @PostMapping("/password/forgotLink")
     public ResponseEntity<DefaultResponse> getForgotPasswordLink(
             @RequestBody @Valid ForgotPasswordRequest forgotPasswordRequest) {
         return ResponseEntity.ok(authService.createForgotPasswordToken(forgotPasswordRequest.email()));
     }
 
+    @Operation(summary = "api.auth.password.reset.summary")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "api.auth.password.reset.200.description",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = DefaultResponse.class))}),
+            @ApiResponse(responseCode = "404", description = "api.auth.password.reset.404.description",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorControllerAdvice.class))}),
+            @ApiResponse(responseCode = "500", description = "api.auth.password.reset.500.description",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorControllerAdvice.class))})
+    })
     @PostMapping("/password/reset")
     public ResponseEntity<DefaultResponse> resetPassword(@RequestBody @Valid PasswordResetRequest passwordResetRequest,
                                                          @RequestParam("token") String token) {
